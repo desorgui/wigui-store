49c1cfa1c5e352766e3bd80daf1137243428b027
Desor Guishny <desorguishny@gmail.com>
2023-02-08 18:25:02 -0500

    Change button add to cart style

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -26,10 +26,15 @@ const CardItem = (props) => {
                 </svg>
               </div>
               <div className="flex items-center justify-between">
-                <span className="text-3xl font-bold text-gray-900 dark:text-white">$ {price}</span>
+                <span className="text-lg font-bold text-gray-900 dark:text-white">$ {price}</span>
                 <NavLink to="#"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add
-                  to cart</NavLink>
+                  className="text-white relative overflow-hidden focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-32 h-12 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <span className="text-center pt-4 bg-gradient-to-r left-0 top-0 from-[#ffffff79] to-[#96969640] z-20 absolute backdrop-blur-md w-full h-full leading-12">Add to cart</span>
+                  <span className="absolute top-0 -left-4 z-10 rounded-full w-20 h-12 transition-colors duration-300 bg-[#ec331a]" />
+                  <span className="absolute top-0 left-4 z-10 rounded-full w-20 h-12 transition-colors duration-300 bg-[#ec1aad]" />
+                  <span className="absolute -top-4 left-16 z-10 rounded-full w-20 h-12 transition-colors duration-300 bg-[#ecbb1a]" />
+                  <span className="absolute top-4 left-16 z-10 rounded-full w-20 h-12 transition-colors duration-300 bg-[#ff1b6b]" />
+                </NavLink>
               </div>
             </div>
         </div>
