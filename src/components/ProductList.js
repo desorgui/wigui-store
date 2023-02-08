97e8e5acbce86298395f21beb32105468936ea3d
Desor Guishny <desorguishny@gmail.com>
2023-02-08 14:15:43 -0500

    Add custom pagination

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,49 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from 'react';
 import { useSelector } from "react-redux";
-import ReactPaginate from 'react-paginate';
 import CardItem from "./CardItem";
+import Pagination from "./Pagination";
 
 const ProductList = () => {
   const products =  useSelector((state) => state.products);
 
-  return (
-    <div className="container mx-auto">
-      <h2 className="text-center text-3xl lg:text-4xl text-primary-dark-blue mb-5 lg:text-left lg:mb-10">Latest Articles</h2>
-      <div className="md:px-4 md:grid md:grid-cols-2 lg:grid-cols-3 gap-5 space-y-4 md:space-y-0">
-        {products.map((product) => (
-            <CardItem {...product} />
-        ))}
-      </div>
-    </div>
-  );
-};
+  const [currentPage, setCurrentPage] = useState(1);
 
-function PaginatedItems({ itemsPerPage }) {
-  const products =  useSelector((state) => state.products);
-  const [itemOffset, setItemOffset] = useState(0);
-  const endOffset = itemOffset + itemsPerPage;
-  const currentItems = products.slice(itemOffset, endOffset);
-  const pageCount = Math.ceil(products.length / itemsPerPage);
-
-  const handlePageClick = (event) => {
-    const newOffset = (event.selected * itemsPerPage) % products.length;
-    setItemOffset(newOffset);
-  };
+  const currentTableData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * 6;
+    const lastPageIndex = firstPageIndex + 6;
+    return products.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage]);
 
   return (
     <>
-      <ProductList />
-      <ReactPaginate
-        breakLabel="..."
-        nextLabel="next >"
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={5}
-        pageCount={pageCount}
-        previousLabel="< previous"
-        renderOnZeroPageCount={null}
+      <div className="container mx-auto">
+        <h2 className="text-center text-3xl lg:text-4xl text-primary-dark-blue mb-5 lg:text-left lg:mb-10">Latest Articles</h2>
+        <div className="md:px-4 md:grid md:grid-cols-2 lg:grid-cols-3 gap-5 space-y-4 md:space-y-0">
+          {currentTableData.map((product) => (
+              <CardItem key={product.id} {...product} />
+          ))}
+        </div>
+      </div>
+      <Pagination
+      className="pagination-bar"
+      currentPage={currentPage}
+      totalCount={products?.length}
+      pageSize={6}
+      onPageChange={page => setCurrentPage(page)}
       />
     </>
   );
-}
+};
 
-export default PaginatedItems;
+export default ProductList;
