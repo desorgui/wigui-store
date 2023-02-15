0fed086a7a5c920e4780fcf088799f9f0a5858f8
Desor Guishny <desorguishny@gmail.com>
2023-02-15 14:30:54 -0500

    Add dynamic product to cart popup

--- src/components/cartPopup.js
+++ src/components/cartPopup.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const CartPopup = (props) => {
 
   const { isOpen, isClose } = props;
 
+  const cartItems = useSelector((state) => state.carts);
 
   return (
     <>
@@ -34,18 +36,20 @@ const CartPopup = (props) => {
                   <div className="mt-8">
                     <div className="flow-root">
                       <ul role="list" className="-my-6 divide-y divide-gray-200">
-                        <li className="flex py-6">
+                        {cartItems.map((product) => {
+                          return (
+                        <li className="flex py-6" key={product.id}>
                           <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                            <img src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg" alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt." className="h-full w-full object-cover object-center" />
+                            <img src={product.thumbnail} alt={product.title} className="h-full w-full object-cover object-center" />
                           </div>
 
                           <div className="ml-4 flex flex-1 flex-col">
                             <div>
                               <div className="flex justify-between text-base font-medium text-gray-900">
                                 <h3>
-                                  <a href="#">Throwback Hip Bag</a>
+                                  <a href="#">{product.title}</a>
                                 </h3>
-                                <p className="ml-4">$90.00</p>
+                                <p className="ml-4">${product.price}</p>
                               </div>
                               <p className="mt-1 text-sm text-gray-500">Salmon</p>
                             </div>
@@ -57,34 +61,9 @@ const CartPopup = (props) => {
                               </div>
                             </div>
                           </div>
-                        </li>
-
-                        <li className="flex py-6">
-                          <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                            <img src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg" alt="Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch." className="h-full w-full object-cover object-center" />
-                          </div>
-
-                          <div className="ml-4 flex flex-1 flex-col">
-                            <div>
-                              <div className="flex justify-between text-base font-medium text-gray-900">
-                                <h3>
-                                  <a href="#">Medium Stuff Satchel</a>
-                                </h3>
-                                <p className="ml-4">$32.00</p>
-                              </div>
-                              <p className="mt-1 text-sm text-gray-500">Blue</p>
-                            </div>
-                            <div className="flex flex-1 items-end justify-between text-sm">
-                              <p className="text-gray-500">Qty 1</p>
-
-                              <div className="flex">
-                                <button type="button" className="font-medium text-indigo-600 hover:text-indigo-500">Remove</button>
-                              </div>
-                            </div>
-                          </div>
-                        </li>
-
-                        {/* <!-- More products... --> */}
+                        </li> 
+                        )
+                      })}
                       </ul>
                     </div>
                   </div>
