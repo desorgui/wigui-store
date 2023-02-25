d362b6796374c8a8e755faa50e2f843325445924
Desor Guishny <desorguishny@gmail.com>
2023-02-25 12:54:20 -0500

    Update footer to remove bad links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../images/logo2.png';
 
 const Footer = () => (
   <footer className="p-4 bg-white rounded-lg shadow md:px-6 xl:px-12 md:py-8 dark:bg-gray-900">
     <div className="sm:flex sm:items-center sm:justify-between">
-      <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0">
+      <Link to="/" className="flex items-center mb-4 sm:mb-0">
         <img src={logo} className="h-8 mr-3" alt="Wigui Store Logo" />
-      </a>
+      </Link>
       <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
         <li>
           <span className="mr-4 hover:underline md:mr-6 ">About</span>
